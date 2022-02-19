ab10077ae1604db54769aed7c7844945481fddf1
Alex Truong <alextruong0901@gmail.com>
2022-02-19 11:58:54 -0800

    add code to extract track info from a playlist

--- src/scripts/data_download.py
+++ src/scripts/data_download.py
@@ -2,11 +2,13 @@
 # Date: 2022-02-11
 # Ref: https://towardsdatascience.com/extracting-song-data-from-the-spotify-api-using-python-b1e79388d50
 
+
 ########## IMPORTS ##########
 import json
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+import pandas as pd
 
 ########## CODE ##########
 
@@ -32,14 +34,61 @@ sp = spotipy.Spotify(
 # Step 2: Extracting Tracks from a Playlist
 
 
+def get_uri_from_link(spotify_link):
+    return spotify_link.split("/")[-1].split("?")[0]
+
+
+def get_artist_info(artist_uri):
+    """Generate information about an artist
+
+    Parameters
+    ----------
+    artist_uri : string
+        a URI of an artist
+
+    Returns
+    -------
+    dictionary
+        a dictionary contains an artist's name, URI, current followers, genres, popularity score
+    """
+    artist_info = sp.artist(artist_uri)
+
+    artist_dict = dict(
+        artist_name=artist_info["name"],
+        artist_uri=artist_uri,
+        artist_followers=artist_info["followers"]["total"],
+        artist_genres=artist_info["genres"],
+        artist_pop=artist_info["popularity"],
+    )
+    return artist_dict
+
+
+def get_track_info_for_playlist(playlist_link):
+    """Get track information for a given playlist"""
+    playlist_uri = get_uri_from_link(playlist_link)
+    results = sp.playlist_tracks(playlist_id=playlist_uri, market="US")
+
+    tracks = dict(
+        track_name=[], track_uri=[], track_pop=[], artist_name=[], artist_uri=[]
+    )
+
+    for track in results["items"]:
+        tracks["track_name"].append(track["track"]["name"])
+        tracks["track_uri"].append(track["track"]["id"])
+        tracks["track_pop"].append(track["track"]["popularity"])
+
+        tracks["artist_name"].append(track["track"]["artists"][0]["name"])
+        tracks["artist_uri"].append(track["track"]["artists"][0]["id"])
+
+    return pd.DataFrame(tracks)
+
+
+# Editorial Playlist: Top Songs - Global (Weekly Music Charts)
 playlist_link = (
     "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
 )
-#%%
-playlist_uri = playlist_link.split("/")[-1].split("?")[0]
 
-
-sp.playlist_tracks(playlist_id=playlist_uri)
+a = get_track_info_for_playlist(playlist_link)
 
 #%%
 
